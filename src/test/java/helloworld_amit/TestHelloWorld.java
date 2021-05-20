7a82cfb6ad3fb72ee3ce7b0a9855c68c1ccb393e
Amit_Sonthalia <amitsonthalia22@gmail.com>
2021-05-20 20:39:47 +0530

    firstcommit

---
+++ src/test/java/helloworld_amit/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld_amit;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("Inside testHelloWorld method.");
+	}
+
+}
